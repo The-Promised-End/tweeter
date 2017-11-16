58da96214b91aae2ad4111cfbf4e2ba9311796ba
The-Promised-End <tom.tk.li@gmail.com>
2017-11-16 20:37:17 +0000

    Character counter changes colors and reverts to black depending on text length

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,13 @@
-$(document).ready(function() {
-  console.log("hello!")
+$(document).ready(function(){
+  var maxLength = 140;
+  $('.new-tweet textarea').keyup(function() {
+  var length = $(this).val().length;
+  var length = maxLength-length;
+  $('.counter').text(length);
+  if(length < 0){
+    $('.counter').css('color', 'red')
+  } else {
+    $('.counter').css('color', 'black')
+  };
 });
+})
